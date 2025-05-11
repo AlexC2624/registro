1a67e3f6ba78f2040404517fc7e9576d3ede79f2
AlexC2624 <alex.cargnin.2006@gmail.com>
2025-05-10 23:37:17 -0300

    remoção de endpoint (bluprint)

--- init.py
+++ init.py
@@ -4,21 +4,18 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)   # Inicia o app do flask
 
-# from animais import animais_bp
-# app.register_blueprint(animais_bp, url_prefix='/animais')
-
 GET, POST = 'GET', 'POST'
 
 os.makedirs('data', exist_ok=True)
 
 @app.route('/', methods= [GET, POST])
 def index():
-    # if request.method == POST:
-    #     acao = request.form.get('acao')
-    #     if acao == 'lote_cadastro':
-    #         return redirect(url_for('lote_cadastro'))
-    #     if acao == 'animal_entrada':
-    #         return redirect(url_for('animais.animal_entrada'))
+    if request.method == POST:
+        acao = request.form.get('acao')
+        if acao == 'lote_cadastro':
+            return redirect(url_for('lote_cadastro'))
+        if acao == 'animal_entrada':
+            return redirect(url_for('animal_entrada'))
     return render_template('index.html')
 
 @app.route('/teste')
