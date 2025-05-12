ac47ecc6e46d21a9f89727bfdee8a54ced89d7ca
AlexC2624 <alex.cargnin.2006@gmail.com>
2025-05-12 12:58:56 -0300

    junção das rotas de animais para /animal/<modo> para Entrada e Saída

--- init.py
+++ init.py
@@ -30,10 +30,10 @@ def index():
         description: Página inicial carregada
     """
     if request.method == POST:
-        acao = request.form.get('acao')
+        return redirect(url_for('animal', modo=request.form.get('modo')))
         # if acao == 'lote_animal': return redirect(url_for('lote_animal'))
-        if acao == 'animal_entrada': return redirect(url_for('animal_entrada'))
-        if acao == 'animal_saida': return redirect(url_for('animal_saida'))
+        # if acao == 'animal_entrada': return redirect(url_for('animal_entrada', ))
+        # if acao == 'animal_saida': return redirect(url_for('animal_saida'))
     return render_template('index.html')
 
 @app.route('/teste')
@@ -105,8 +105,8 @@ def json_animal(categoria):
 
     return render_template('cadastro_json_animais.html', categoria=categoria, status=status)
 
-@app.route('/animal_entrada', methods= [GET, POST])
-def animal_entrada():
+@app.route('/animal/<modo>', methods= [GET, POST])
+def animal(modo):
     """
     Cadastro de entrada de animais.
     ---
@@ -146,142 +146,99 @@ def animal_entrada():
       200:
         description: Página de entrada de animais renderizada
     """
-    lote_opcoes = ManagerJSON('animais.json')
-    lote_opcoes = lote_opcoes.obter_dado('lote')
-    lote_opcoes = [lote_opcoes[i]['nome'] for i in lote_opcoes.keys()]
-    if lote_opcoes == []:
-        status = 'Nenhum lote de animais cadastrado'
-        return render_template('animal_entrada.html', status=status)
-
-    raca_opcoes = ManagerJSON('animais.json')
-    raca_opcoes = raca_opcoes.obter_dado('raca')
-    raca_opcoes = [raca_opcoes[i]['nome'] for i in raca_opcoes.keys()]
-    if raca_opcoes == []:
-        status = 'Nenhuma raca de animais cadastrado'
-        return render_template('animal_entrada.html', status=status)
-
-    fornecedor_opcoes = ManagerJSON('animais.json')
-    fornecedor_opcoes = fornecedor_opcoes.obter_dado('fornecedor')
-    fornecedor_opcoes = [fornecedor_opcoes[i]['nome'] for i in fornecedor_opcoes.keys()]
-    if fornecedor_opcoes == []:
-        status = 'Nenhum fornecedor de animais cadastrado'
-        return render_template('animal_entrada.html', status=status)
-    status = None
-
-    if request.method == POST:
-        lote = request.form['lote']
-        raca = request.form['raca']
-        data_nascimento = request.form['data_nascimento']
-        fornecedor = request.form['fornecedor']
-        data_entrada = request.form['data_entrada']
-        peso_entrada = request.form['peso_entrada']
-        valor_entrada = request.form['valor_entrada']
-
-        # Caminho do arquivo CSV
-        arquivo = 'data/animal_entrada.csv'
-
-        # Criar dicionário com os dados recebidos
-        novo_animal = {
-            'lote': lote,
-            'raca': raca,
-            'data_nascimento': data_nascimento,
-            'fornecedor': fornecedor,
-            'data_entrada': data_entrada,
-            'peso_entrada': peso_entrada,
-            'valor_entrada': valor_entrada
-        }
-
-        # Verifica se o arquivo já existe
-        banco = ManagerCSV(arquivo, list(novo_animal.keys()))
-        banco.adicionar(novo_animal)
-
-        status = 'Salvo com sucesso!'
-
-    return render_template(
-        'animal_entrada.html',
-        lote_opcoes = lote_opcoes,
-        raca_opcoes = raca_opcoes,
-        fornecedor_opcoes = fornecedor_opcoes,
-        status = status
-    )
-
-@app.route('/animal_saida', methods= [GET, POST])
-def animal_saida():
-    """
-    Cadastro de entrada de animais.
-    ---
-    methods:
-      - GET
-      - POST
-    parameters:
-      - name: lote
-        in: formData
-        type: string
-        required: true
-      - name: raca
-        in: formData
-        type: string
-        required: true
-      - name: data_nascimento
-        in: formData
-        type: string
-        required: true
-      - name: fornecedor
-        in: formData
-        type: string
-        required: true
-      - name: data_entrada
-        in: formData
-        type: string
-        required: true
-      - name: peso_entrada
-        in: formData
-        type: string
-        required: true
-      - name: valor_entrada
-        in: formData
-        type: string
-        required: true
-    responses:
-      200:
-        description: Página de entrada de animais renderizada
-    """
-    cliente_opcoes = ManagerJSON('animais.json')
-    cliente_opcoes = cliente_opcoes.obter_dado('cliente')
-    cliente_opcoes = [cliente_opcoes[i]['nome'] for i in cliente_opcoes.keys()]
-    if cliente_opcoes == []:
-        status = 'Nenhum cliente de animal cadastrado'
-        return render_template('animal_saida.html', status=status)
     
     status = None
+    json = ManagerJSON('animais.json')
 
-    if request.method == POST:
-        idx_entrada = request.form['idx_entrada']
-        cliente = request.form['cliente']
-        data_saida = request.form['data_saida']
-        peso_saida = request.form['peso_saida']
-        valor_saida = request.form['valor_saida']
+    if modo == 'Entrada':
+        if request.method == POST:
+            lote = request.form['lote']
+            raca = request.form['raca']
+            data_nascimento = request.form['data_nascimento']
+            fornecedor = request.form['fornecedor']
+            data_entrada = request.form['data_entrada']
+            peso_entrada = request.form['peso_entrada']
+            valor_entrada = request.form['valor_entrada']
 
-        # Caminho do arquivo CSV
-        arquivo = 'data/animal_saida.csv'
+            # Caminho do arquivo CSV
+            arquivo = 'data/animal_entrada.csv'
 
-        # Criar dicionário com os dados recebidos
-        novo_animal = {
-            'idx_entrada': idx_entrada,
-            'cliente': cliente,
-            'data_saida': data_saida,
-            'peso_saida': peso_saida,
-            'valor_saida': valor_saida
-        }
+            # Criar dicionário com os dados recebidos
+            novo_registro = {
+                'lote': lote,
+                'raca': raca,
+                'data_nascimento': data_nascimento,
+                'fornecedor': fornecedor,
+                'data_entrada': data_entrada,
+                'peso_entrada': peso_entrada,
+                'valor_entrada': valor_entrada
+            }
 
-        # Verifica se o arquivo já existe
-        banco = ManagerCSV(arquivo, list(novo_animal.keys()))
-        banco.adicionar(novo_animal)
+            banco = ManagerCSV(arquivo, list(novo_registro.keys()))
+            banco.adicionar(novo_registro)
 
-        status = 'Salvo com sucesso!'
+            status = 'Salvo com sucesso!'
 
+        lote_opcoes = json.obter_dado('lote')
+        lote_opcoes = [lote_opcoes[i]['nome'] for i in lote_opcoes.keys()]
+        if lote_opcoes == []:
+            status = 'Nenhum lote de animais cadastrado'
+            return render_template('animal.html', status=status)
+
+        raca_opcoes = json.obter_dado('raca')
+        raca_opcoes = [raca_opcoes[i]['nome'] for i in raca_opcoes.keys()]
+        if raca_opcoes == []:
+            status = 'Nenhuma raca de animais cadastrado'
+            return render_template('animal.html', status=status)
+
+        fornecedor_opcoes = json.obter_dado('fornecedor')
+        fornecedor_opcoes = [fornecedor_opcoes[i]['nome'] for i in fornecedor_opcoes.keys()]
+        if fornecedor_opcoes == []:
+            status = 'Nenhum fornecedor de animais cadastrado'
+            return render_template('animal.html', status=status)
+    elif modo == 'Saída':
+        if request.method == POST:
+            idx_entrada = request.form['idx_entrada']
+            cliente = request.form['cliente']
+            data_saida = request.form['data_saida']
+            peso_saida = request.form['peso_saida']
+            valor_saida = request.form['valor_saida']
+
+            # Caminho do arquivo CSV
+            arquivo = 'data/animal_saida.csv'
+
+            # Criar dicionário com os dados recebidos
+            novo_registro = {
+                'idx_entrada': idx_entrada,
+                'cliente': cliente,
+                'data_saida': data_saida,
+                'peso_saida': peso_saida,
+                'valor_saida': valor_saida
+            }
+
+            banco = ManagerCSV(arquivo, list(novo_registro.keys()))
+            banco.adicionar(novo_registro)
+
+            status = 'Salvo com sucesso!'
+
+        cliente_opcoes = json.obter_dado('cliente')
+        cliente_opcoes = [cliente_opcoes[i]['nome'] for i in cliente_opcoes.keys()]
+        if cliente_opcoes == []:
+            status = 'Nenhum cliente de animal cadastrado'
+            return render_template('animal.html', status=status)
+
+    if modo == 'Entrada':
+        return render_template(
+            'animal.html',
+            modo = modo,
+            lote_opcoes = lote_opcoes,
+            raca_opcoes = raca_opcoes,
+            fornecedor_opcoes = fornecedor_opcoes,
+            status = status
+        )
     return render_template(
-        'animal_saida.html',
+        'animal.html',
+        modo = modo,
         cliente_opcoes = cliente_opcoes,
         status = status
     )
