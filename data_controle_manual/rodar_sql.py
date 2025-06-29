f9e0a799170fd41595514207975d54c446444c13
AlexC2624 <alex.cargnin.2006@gmail.com>
2025-06-29 11:21:35 -0300

    adiciona configuração de logging para monitoramento de execução de SQL

--- data_controle_manual/rodar_sql.py
+++ data_controle_manual/rodar_sql.py
@@ -1,6 +1,7 @@
 import sqlite3
 import time
 import os
+import logging
 
 # --- Configurações ---
 DB_FILE = 'data/dados.db'  # Nome do arquivo do seu banco de dados SQLite
@@ -14,6 +15,27 @@ def execute_sql_script(db_path, sql_script_path):
     try:
         conn = sqlite3.connect(db_path)
         cursor = conn.cursor()
+
+        # 1. Configurar o logger no __init__
+        logger = logging.getLogger(__name__)
+        # Define o nível mínimo de log para este logger
+        logger.setLevel(logging.DEBUG)
+
+        # Cria um FileHandler para escrever no arquivo de log
+        log_file_path = 'data/log_db.log'
+        file_handler = logging.FileHandler(log_file_path)
+        file_handler.setLevel(logging.DEBUG) # Nível mínimo para o arquivo de log
+
+        # Cria um Formatter para definir o formato das mensagens
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(formatter)
+
+        # Adiciona o handler ao logger, se ele ainda não foi adicionado
+        # Isso previne a adição de múltiplos handlers em chamadas subsequentes do __init__
+        if not logger.handlers:
+            logger.addHandler(file_handler)
+        
+        conn.set_trace_callback(lambda comando_sql: logger.debug(f'SQL Executado: {comando_sql}'))
 
         print(f"Executando comandos do script '{sql_script_path}'...") # Mensagem adicionada para depuração
 
